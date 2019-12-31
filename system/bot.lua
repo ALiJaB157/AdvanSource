279fab5c297b5c2fbcc482b3fd481a8aa3a2386a
ALiJaB157 <alijabare157@gmail.com>
2019-12-31 08:40:31 -0800

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,7 +234,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {272553396,104344213,tonumber(our_id)},--Sudo users
+    sudo_users = {376998999},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
